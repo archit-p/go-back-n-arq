85275b7a7adef2d4e0fac2ffd9d45ccc93ed55fc
mrigankshah <37183789+mrigankshah@users.noreply.github.com>
2018-04-06 17:17:14 +0530

    Update gon.m

--- matlab/gon.m
+++ matlab/gon.m
@@ -6,3 +6,13 @@ pt=1;
 flag=0;
 flag2=0;
 a=1:n;
+while flag==0
+  if flag2==0
+    %Transmitting The Frames In A Window
+    for i=1:w
+      fprintf('Frame %d Transmitted\n',a(pt));
+      pt=pt+1;
+    end
+    flag2=1;
+  end
+  s = randi(10,1,1);
