8d43c9d00a467a28ee3f206afb89ba43006ab537
123tarunanand <anandtarun2@gmail.com>
2018-04-14 00:22:03 +0530

    Errors in matlab code

--- matlab/src/final/prob.m
+++ matlab/src/final/prob.m
@@ -2,15 +2,16 @@ lower = 0;
 numreq = 100;
 upper = numreq * 10;
 threshold = 0.28 * upper;
-prev = "False";
+%prev = "False";
 while(numreq > 1)
   guess = randi([lower, upper], 1)
   if(guess > threshold)
-    lower += 10;
+    lower = lower + 10;
   else
-    lower -= 20;
+    lower = lower - 20;
   end
   if(lower < 0)
     lower = 0;
-  numreq -= 1;
+  end
+  numreq = numreq - 1;
 end
