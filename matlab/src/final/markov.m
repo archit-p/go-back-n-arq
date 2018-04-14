31625924e09cab960ea59577e0160ba0807ebe25
Archit Pandey <architpandeynitk@gmail.com>
2018-04-14 15:42:40 +0530

    Added new markov channel with better approximation

--- matlab/src/final/markov.m
+++ matlab/src/final/markov.m
@@ -1,24 +1,34 @@
-function result = markov(noiseHist)
+function result = markov(threshold, noiseHist)
   # functin to return markov probability function
-  # prevone is the last value
-  # prevtwo is the second last value
+  # noiseHist if the history of noiseHist
+  # threshold is the border value
  
   # first set the lower bound to 0 and upper bound to 100
   # set threshold to 0.28 * upper
   lower = 0;          
   upper = 100;
-  threshold = 0.28 * upper;
   count = 0;
   prevNoise = length(noiseHist);
+  # loop to count the number of previous noise bellow the threshold
   while(prevNoise > 0 && noiseHist(prevNoise) <= threshold)
     count += 1;
     prevNoise = prevNoise - 1;
   end
-  count
-  val = 1;
+  # predict a value
+  pred = randi([lower, upper], 1);
+  # decrement the value by 5 * count to increase noise probability
   while(count > 0)
     count -= 1;
-    val = val * 1.02;
+    pred -= 5;
   end
-  val
+  
+  # neaten the values by removing exceptional cases
+  if(pred < 0)
+    pred = 0;
+  end
+  if(pred > upper)
+    pred = upper;
+  end
+  #return the prediction
+  result = pred;
 endfunction
