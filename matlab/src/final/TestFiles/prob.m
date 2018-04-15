ebfe5c5575f81db6ac66bf2c62d822cf5d55e31f
mrigankshah <37183789+mrigankshah@users.noreply.github.com>
2018-04-15 19:13:14 +0530

    Update prob.m

--- matlab/src/final/TestFiles/prob.m
+++ matlab/src/final/TestFiles/prob.m
@@ -1,9 +1,13 @@
 function result = prob(prev)
+%Implementing a basic Markov channel
+
   lower = 0;
   numreq = 100;
   upper = numreq * 10;
   threshold = 0.28 * upper;
   %prev = "False";
+  
+  %Considering Various Cases
   while(numreq > 1)
     guess = randi([lower, upper], 1)
     if(guess > threshold)
